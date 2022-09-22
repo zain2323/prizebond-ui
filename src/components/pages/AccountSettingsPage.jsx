b55c9c0b88052d831c52d177cef4f2cc314e6412
Zain <zainsiddiqui2323@gmail.com>
2022-09-22 23:15:02 +0500

    completed account settings page

--- src/components/pages/AccountSettingsPage.jsx
+++ src/components/pages/AccountSettingsPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from 'react';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -10,11 +10,13 @@ import { _ } from "gridjs-react";
 import EditRoundedIcon from '@mui/icons-material/EditRounded';
 import Center from "../utils/Center";
 import { useUser } from '../../contexts/UserProvider'
+import { useApi } from '../../contexts/ApiProvider'
 
 
 export default function AccountSettingsPage() {
     const { user } = useUser()
-    console.log("USER", user)
+    const api = useApi()
+    
     const editButton = _(
         <>
             <a className="EditButton" onClick={()=>alert("clicked")}>
@@ -24,15 +26,33 @@ export default function AccountSettingsPage() {
         </>
 
     )
+    const [info, setInfo] = React.useState([])
+
+    async function fetchInfo() {
+        const response = await api.get("/user/bonds/info");
+        setInfo(response.ok ? response.body : null);
+    }
     
-    
-    const heading = _(
-        <Typography variant="h6">Name</Typography>
-    )
-    const styles = {
-        table: {
-            border: 'none'
-        },
+    React.useEffect(() => {
+        fetchInfo()
+    }, [api])
+
+    function getTotalBonds() {
+        let sum = 0
+        
+        for (let i = 0; i < info.length; i++) {
+            sum += info[i].total
+        }
+        return sum
+    }
+
+    function getTotalWorth() {
+        let worth = 0
+        
+        for (let i = 0; i < info.length; i++) {
+            worth += info[i].worth
+        }
+        return worth
     }
 
     return (
@@ -44,9 +64,8 @@ export default function AccountSettingsPage() {
                 <Typography sx={{ mb: 5 }} variant="h3">General Account Settings</Typography>
             </Center>
             <GridTable
-                style={styles}
                 data={[
-                    [_(<Typography variant="h6">Name</Typography>), _(<Typography variant="body1">{user.name}</Typography>), editButton],
+                    [_(<Typography variant="h6">Name</Typography>), _(<Typography sx={{textTransform: "capitalize"}} variant="body1">{user.name}</Typography>), editButton],
                     [_(<Typography variant="h6">Email</Typography>), _(<Typography variant="body1">{user.email}</Typography>), editButton],
                     [_(<Typography variant="h6">Password</Typography>), _(<Typography variant="body1">**********</Typography>), editButton],
                     [_(<Typography variant="h6">Confirmed</Typography>), _(<Typography variant="body1">{user.confirmed ? "Confirmed": "Not Confirmed"}</Typography>), editButton],
@@ -54,6 +73,27 @@ export default function AccountSettingsPage() {
                 ]}
 
             />
+            <Center>
+                <Typography sx={{ mb: 5, mt: 5 }} variant="h3">Your Bonds Information</Typography>
+            </Center>
+            <GridTable
+                columns={["Denomination", "Total Numbers", "Worth (In Rs)"]}
+                data={info.map(data=> {
+                    return [_(<Typography variant="h6">Rs.{data.price}</Typography>), 
+                            _(<Typography variant="body1">{parseInt(data.total) < 9 ? "0"+data.total: data.total}</Typography>), 
+                            _(<Typography variant="body1">Rs.{data.worth}</Typography>)]
+                })}
+            />
+             <Center>
+                <GridTable
+                columns={["Number of bonds", "Total Investment"]}
+                data={[
+                    [_(<Typography variant="h6">{getTotalBonds()}</Typography>), _(<Typography variant="h6">Rs.{getTotalWorth()}</Typography>)]
+                ]}
+
+            />
+            </Center>
+            
         </Container>
     )
 }
