6be00a2a989f923db4e953aca0af490ef5189473
Zain <zainsiddiqui2323@gmail.com>
2022-09-27 00:19:26 +0500

    integrated search results page with backend

--- src/components/pages/ResultsPage.jsx
+++ src/components/pages/ResultsPage.jsx
@@ -6,35 +6,33 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
+import {useApi} from "../../contexts/ApiProvider";
 
 export default function ResultsPage() {
+    const api = useApi();
+    const [bonds, setBonds] = React.useState([])
+
+    async function fetchBonds() {
+        const response = await api.get("/result")
+        console.log(response.body)
+        setBonds(response.ok ? response.body : [])
+    }
+
+    React.useEffect(() => {
+        fetchBonds()
+    }, [api])
+
     return (
         <Container>
             <Typography variant="h4" sx={{mt: 2, mb: 2}}>
                 Search your results
             </Typography>
             <Grid
-                data={[
-                    ['750', '004208', '17-01-2022', "first", '12', 'Peshawar'],
-                    ['750', '511294', '17-01-2022', "second", '12','Peshawar'],
-                    ['750', '549996', '17-01-2022', "second", '12','Peshawar'],
-                    ['750', '904546', '17-01-2022', "second", '12','Peshawar'],
-                    ['100', '983274', '15-01-2022', "first", '12','Hyderabad'],
-                    ['100', '074566', '15-01-2022', "second", '12','Hyderabad'],
-                    ['100', '259751', '15-01-2022', "second", '12','Hyderabad'],
-                    ['100', '885455', '15-01-2022', "second", '12','Hyderabad'],
-                    ['750', '004208', '17-01-2022', "first", '12', 'Peshawar'],
-                    ['750', '511294', '17-01-2022', "second", '12','Peshawar'],
-                    ['750', '549996', '17-01-2022', "second", '12','Peshawar'],
-                    ['750', '904546', '17-01-2022', "second", '12','Peshawar'],
-                    ['100', '983274', '15-01-2022', "first", '12','Hyderabad'],
-                    ['100', '074566', '15-01-2022', "second", '12','Hyderabad'],
-                    ['100', '259751', '15-01-2022', "second", '12','Hyderabad'],
-                    ['100', '885455', '15-01-2022', "second", '12','Hyderabad'],
-                ]}
+                data={bonds.map(bond => {
+                    return [bond.denomination, bond.serial, bond.draw_date, bond.position, bond.prize, bond.draw_num, bond.location]
+                })}
                 autoWidth={false}
-                columns={['Denomination', 'Serial', 'Date', 'Position', 'Draw no', 'City']}
+                columns={['Denomination', 'Serial', 'Date', 'Position', 'Prize', 'Draw no', 'City']}
                 search={true}
                 sort={true}
                 pagination={{
